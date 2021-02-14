9a9068bf3e65f35f3379483a849ac32b2a521846
松野浩士 <amanojac52@gmail.com>
2021-02-15 02:02:17 +0900

    [1-3]slug()のテスト・実装を回答に合わせて修正

--- app/app/Models/VacancyLevel.php
+++ app/app/Models/VacancyLevel.php
@@ -15,24 +15,27 @@ class VacancyLevel extends Model
 
     public function mark(): string
     {
-        if ($this->remainingCount === 0) {
-            return '×';
-        }
-        if ($this->remainingCount < 5) {
-            return '△';
-        }
-        return '◎';
+        //slug()と条件分岐が同じなので、安定性の高いslug()を基準としたメソッドにする
+        $marks = [
+            'empty' => '×',
+            'few' => '△',
+            'enough' => '◎',
+        ];
+        $slug = $this->slug();
+        //assertionがfalseであるかどうかを調べる
+        assert(isset($marks[$slug]), new \DomainException('invalid slug value'));
+
+        return $marks[$slug];
     }
 
     public function slug(): string
     {
-        //マークとCSSクラスの対応表
-        $markCorrespondSlug = [
-            '×' => 'empty',
-            '△' => 'few',
-            '◎' => 'enough',
-        ];
-        $slug = $markCorrespondSlug[$this->mark()];
-        return $slug;
+        if ($this->remainingCount === 0) {
+            return 'empty';
+        }
+        if ($this->remainingCount < 5) {
+            return 'few';
+        }
+        return 'enough';
     }
 }
